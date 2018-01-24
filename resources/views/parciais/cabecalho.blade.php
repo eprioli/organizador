c91b0110db5bccf06a66e746745aef6bc0f87cd7
Eduardo Prioli Novaes <eprioli@gmail.com>
2018-01-24 19:51:01 -0200

    Criação dos models: Link e Pasta com flag -a
    Criei também o font-awesome.min.css

--- resources/views/parciais/cabecalho.blade.php
+++ resources/views/parciais/cabecalho.blade.php
@@ -5,7 +5,7 @@
 	   <title>@yield('aba')</title>
   	 {{ Html::style('css/bootstrap.min.css') }}
 	   {{ Html::style('css/bootstrap-theme.min.css') }}
-	   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	   {{ Html::style('css/font-awesome.min.css') }}
 	   {{ Html::style('css/master.css') }}
      @yield('stylesheets')
   </head>
